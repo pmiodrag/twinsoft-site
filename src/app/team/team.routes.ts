2e322df85fbbf4312a66b6b856e9852c45162ac5
miodrag <mpavkovic@gmail.com>
2017-03-13 19:57:47 +0100

    making production build

--- src/app/team/team.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { TeamComponent } from './team.component';
-
-export const TeamRoutes: Routes = [
-  { path: 'team',  component: TeamComponent }
-];
